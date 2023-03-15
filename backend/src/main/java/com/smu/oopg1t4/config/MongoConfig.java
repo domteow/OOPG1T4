2b9aa825a4d4acc4fba0c46da7fb4cb193cdf70f
kelvinjye <kelvin.yapjieen@gmail.com>
2023-03-15 22:08:40 +0800

    Form 1

--- backend/src/main/java/com/smu/oopg1t4/config/MongoConfig.java
+++ backend/src/main/java/com/smu/oopg1t4/config/MongoConfig.java
@@ -52,40 +52,39 @@ public class MongoConfig {
             // ------------------Fields-----------------------
             //QLI-SHSP-10-F01 NEW VENDOR ASSESSMENT FORM FIELDS
             //Questionnaire 1 for NEW VENDOR ASSESSMENT FORM
-            Field f1 = new Field("Company Name", "text", null);
-            Field f2 = new Field("Company Registration No", "text", null);
-//            Field f2 = new Field(2, "Attendance in Safety Meeting", "radio", List.of("1", "2", "3", "4", "5"));
-            Field f3 = new Field( "GST Registered", "radio", List.of("Yes", "No"));
-            Field f4 = new Field("Office Address", "text", null);
-            Field f5 = new Field("Type of business License / Registration", "radio", List.of("a. Sole proprietorship", "b. Limited Company", "c. Partnership Agreement", "d. Others"));
-            Field f6 = new Field("Type of business License / Registration (Others) (Fill as NIL if not applicable)", "text", null);
+            Field f1 = new Field("Company Name", "text");
+            Field f2 = new Field("Company Registration No", "text");
+            Field f3 = new Field( "GST Registered", "radio", List.of("Yes", "No"), false);
+            Field f4 = new Field("Office Address", "text");
+            Field f5 = new Field("Type of business License / Registration", "radio", List.of("Sole proprietorship", "Limited Company", "Partnership Agreement"), true);
+//            Field f6 = new Field("Type of business License / Registration (Others) (Fill as NIL if not applicable)", "text");
             Field f7 = new Field("Contact Person", "subheader", null );
-            Field f8 = new Field("Name", "text", null);
-            Field f9 = new Field("Tel", "text", null);
-            Field f10 = new Field("Designation", "text", null);
-            Field f11 = new Field("Nature of Business ", "radio", List.of("Manufacturing", "Agent/dealer", "Distributor", "Others"));
-            Field f12 = new Field("Nature of Business (Others) (Fill as NIL if not applicable)", "text", null);
-            Field f13 = new Field("Product/Services", "text", null);
+            Field f8 = new Field("Name", "text");
+            Field f9 = new Field("Tel", "text");
+            Field f10 = new Field("Designation", "text");
+            Field f11 = new Field("Nature of Business ", "radio", List.of("Manufacturing", "Agent/dealer", "Distributor"), true);
+//            Field f12 = new Field("Nature of Business (Others) (Fill as NIL if not applicable)", "text", null);
+            Field f13 = new Field("Product/Services", "text");
 
             //Questionnaire 2 for NEW VENDOR ASSESSMENT FORM
-            Field f14 = new Field("ISO 9001 Certification (if present, type in Certification Body)", "text", null);
-            Field f15 = new Field("Accreditation of Laboratory (if present, type in Accreditation Body)", "text", null);
-            Field f16 = new Field("Product Certification (if present, type in Product Markings (e.g. PSB, UL, TUV):", "text", null);
-            Field f17 = new Field("Site Evaluation Results", "radio", List.of("Satisfactory", "Unsatisfactory"));
-            Field f18 = new Field("Results of Samples/Product Evaluation", "radio", List.of("Satisfactory", "Unsatisfactory"));
-            Field f19 = new Field("Results of First Deal", "radio", List.of("Satisfactory", "Unsatisfactory"));
-            Field f20 = new Field("Track Record Review/ Customer Reference", "radio", List.of("Satisfactory", "Unsatisfactory"));
-            Field f21 = new Field("Others (e.g. commercial, sole supplier, customer specified, franchise etc.)", "text", null);
+            Field f14 = new Field("ISO 9001 Certification (if present, type in Certification Body)", "text");
+            Field f15 = new Field("Accreditation of Laboratory (if present, type in Accreditation Body)", "text");
+            Field f16 = new Field("Product Certification (if present, type in Product Markings (e.g. PSB, UL, TUV):", "text");
+            Field f17 = new Field("Site Evaluation Results", "radio", List.of("Satisfactory", "Unsatisfactory"), false);
+            Field f18 = new Field("Results of Samples/Product Evaluation", "radio", List.of("Satisfactory", "Unsatisfactory"), false);
+            Field f19 = new Field("Results of First Deal", "radio", List.of("Satisfactory", "Unsatisfactory"), false);
+            Field f20 = new Field("Track Record Review/ Customer Reference", "radio", List.of("Satisfactory", "Unsatisfactory"), false);
+            Field f21 = new Field("Others (e.g. commercial, sole supplier, customer specified, franchise etc.)", "text");
 
             //Questionnaire 3 for NEW VENDOR ASSESSMENT FORM
-            Field f22 = new Field("RESULT OF EVALUATION", "radio", List.of("APPROVED", "NOT APPROVED"));
-            Field f23 = new Field("Evaluated by", "text", null);
-            Field f24 = new Field("Signature (Full Name)", "text", null);
-            Field f25 = new Field("Approved by Director", "text", null);
-            Field f26 = new Field("Effective date", "text", null);
+            Field f22 = new Field("RESULT OF EVALUATION", "radio", List.of("APPROVED", "NOT APPROVED"), false);
+            Field f23 = new Field("Evaluated by", "text");
+            Field f24 = new Field("Signature (Full Name)", "text");
+            Field f25 = new Field("Approved by Director", "text");
+            Field f26 = new Field("Effective date", "text");
 
 
-            List<Field> fields = List.of(f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13,f14,f15,f16,f17,f18,f19,f20,f21,f22,f23,f24,f25,f26);
+            List<Field> fields = List.of(f1,f2,f3,f4,f5,f7,f8,f9,f10,f11,f13,f14,f15,f16,f17,f18,f19,f20,f21,f22,f23,f24,f25,f26);
             int fieldId = 1;
             for (Field field: fields){
                 field.setId(fieldId);
@@ -101,7 +100,7 @@ public class MongoConfig {
 
             //Questionnaire 1 for NEW VENDOR ASSESSMENT FORM
             ArrayList<Field> fields1 = new ArrayList<>();
-            fields1.addAll(List.of(f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13));
+            fields1.addAll(List.of(f1,f2,f3,f4,f5,f7,f8,f9,f10,f11,f13));
             Questionnaire q1 = new Questionnaire("Vendor Information", fields1, "Vendor");
 
             //Questionnaire 2 for NEW VENDOR ASSESSMENT FORM
@@ -160,12 +159,12 @@ public class MongoConfig {
                 DatabaseSequence userSequence = new DatabaseSequence("user_sequence",3);
                 databaseSequenceRepository.save(userSequence);
             }
-            if (!databaseSequenceRepository.findById("field_sequence").isPresent() || databaseSequenceRepository.findById("field_sequence").get().getSeq() < 3){
-                DatabaseSequence fieldSequence = new DatabaseSequence("field_sequence",3);
+            if (!databaseSequenceRepository.findById("field_sequence").isPresent() || databaseSequenceRepository.findById("field_sequence").get().getSeq() < 24){
+                DatabaseSequence fieldSequence = new DatabaseSequence("field_sequence",24);
                 databaseSequenceRepository.save(fieldSequence);
             }
-            if (!databaseSequenceRepository.findById("questionnaire_sequence").isPresent() || databaseSequenceRepository.findById("questionnaire_sequence").get().getSeq() < 1){
-                DatabaseSequence questionnaireSequence = new DatabaseSequence("questionnaire_sequence",1);
+            if (!databaseSequenceRepository.findById("questionnaire_sequence").isPresent() || databaseSequenceRepository.findById("questionnaire_sequence").get().getSeq() < 3){
+                DatabaseSequence questionnaireSequence = new DatabaseSequence("questionnaire_sequence",3);
                 databaseSequenceRepository.save(questionnaireSequence);
             }
             if (!databaseSequenceRepository.findById("form_sequence").isPresent() || databaseSequenceRepository.findById("form_sequence").get().getSeq() < 1){
